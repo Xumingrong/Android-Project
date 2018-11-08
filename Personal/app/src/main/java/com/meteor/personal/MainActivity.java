bc6ff921c48b7a11cf4a0d489ba8efc258744f83
Meteor <15537171227@163.com>
2018-11-08 16:21:01 +0800

    代码注释

--- Personal/app/src/main/java/com/meteor/personal/MainActivity.java
+++ Personal/app/src/main/java/com/meteor/personal/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //views
+        //view
         setContentView(R.layout.activity_main);
     }
 }
